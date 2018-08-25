72eec16e7f1ce0f35ab5aa8f1944dfa1dc8c2c79
Russell Wakefield <RJWakefield@gmail.com>
2018-08-25 14:43:20 -0400

    CRUD Placeholders for Player
    * Added a table to display players in the index page.
    * Added actions to the table to perform edit and show actions
    * Added a button to create new players
    * Added a sidebar to link to players index

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,21 @@
 class PlayersController < ApplicationController
+  helper_method :players
+
   def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  private
+
+  def players
+    Player.all
   end
 end
